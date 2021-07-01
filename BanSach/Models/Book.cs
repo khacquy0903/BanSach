b47ac8af9dac1cd55b2aaf65baa22ae002e53d1e
khacquy0903 <quy.khac.bui@gmail.com>
2021-07-01 18:18:16 +0700

    Initial commit

---
+++ BanSach/Models/Book.cs
@@ -0,0 +1,33 @@
+namespace BanSach.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Tittle { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Image { get; set; }
+
+        public int Price { get; set; }
+    }
+}
